b9f8c63d07a7d17d91e7de3e4a00f9ef075ebe51
tarbielee <tarbieletsoalo@gmail.com>
2020-04-02 12:38:19 +0200

    refactored-code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
- app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.listen(3000, function(){
+    console.log('server started on port 3000...');
+});
  
